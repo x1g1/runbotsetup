185bea84dba918dbbde58895d5519785c0e4b4d2
Ralf Müller <61875381+x1g1@users.noreply.github.com>
2021-12-27 11:45:54 +0100

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -3,4 +3,5 @@
 sudo apt install git python3-dev virtualenv python3-virtualenv gcc libsasl2-dev libldap2-dev
 sudo apt-get install libxml2-dev libxslt-dev python-dev
 sudo apt-get install libjpeg-dev zlib1g-dev
-sudo apt install libpq-dev
+sudo apt-get install libpq-dev
+sudo apt-get install python3-matplotlib
