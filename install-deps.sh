411d1686ccc54cd0073f969f6384c634796668c9
Ralf Müller <61875381+x1g1@users.noreply.github.com>
2021-12-27 11:25:53 +0100

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -2,3 +2,4 @@
 
 sudo apt install git python3-dev virtualenv python3-virtualenv gcc libsasl2-dev libldap2-dev
 sudo apt-get install libxml2-dev libxslt-dev python-dev
+sudo apt-get install libjpeg-dev zlib1g-dev
